be14d5dfac81810e1b702fe5f97559884d0d2c93
codedamage <codedamage@protonmail.com>
2020-04-06 19:15:59 +0300

    Cleaned DB data

--- app/config/config.php
+++ app/config/config.php
@@ -2,13 +2,13 @@
 //DB Params
 
 define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 't_mvc');
+define('DB_USER', '_USER');
+define('DB_PASS', '_PASS');
+define('DB_NAME', '__DBNAME');
 
 //App root
 define('APPROOT', dirname(dirname(__FILE__)));
 //URL Root
-define('URLROOT', 'http://thinblog.loc/thinblog');
+define('URLROOT', '__YOUR_PROJECT_URL');
 //Sitename
-define('SITENAME', 'ThinBlog');
+define('SITENAME', '__YOUR_SITENAME');
